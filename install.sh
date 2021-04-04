85c199add44508718119e9eccffa3310ed3e697f
Skyfiler12 <80471397+Skyfiler12@users.noreply.github.com>
2021-04-05 01:50:45 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,33 @@
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+
+add-apt-repository -y ppa:chris-lea/redis-server
+
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+
+apt update
+
+apt-add-repository universe
+
+apt -y install php8.0 php8.0-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+mkdir -p /var/www/pterodactyl
+cd /var/www/pterodactyl
+
+curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+tar -xzvf panel.tar.gz
+chmod -R 755 storage/* bootstrap/cache/
+
+
+
+cp .env.example .env
+composer install --no-dev --optimize-autoloader
+php artisan p:environment:setup
+
+
+
+
+php artisan key:generate --force
